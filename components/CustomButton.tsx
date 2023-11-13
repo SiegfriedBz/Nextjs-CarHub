18b56630a4a5a5d0e9891027b0361b5bc70bc6de
siegfriedbz <siegfried.bozza@yahoo.com>
2023-11-13 19:26:02 +0100

    add CustomButton

---
+++ components/CustomButton.tsx
@@ -0,0 +1,42 @@
+'use client'
+
+import Image from 'next/image'
+import { twMerge } from 'tailwind-merge'
+import type { CustomButtonProps } from '@/types'
+
+const CustomButton = ({
+  type = 'button',
+  content,
+  className = '',
+  src,
+  alt = '',
+  disabled = false,
+  handleClick,
+}: CustomButtonProps) => {
+  return (
+    <button
+      type={type}
+      onClick={handleClick}
+      className={twMerge(
+        'btn flex max-w-max items-center justify-between',
+        `${className} ${
+          disabled ? 'cursor-not-allowed opacity-50 hover:scale-100' : ''
+        }`
+      )}
+    >
+      <span>{content}</span>
+
+      {src && (
+        <Image
+          src={src}
+          width={32}
+          height={32}
+          className='h-full w-full object-cover'
+          alt={alt}
+        />
+      )}
+    </button>
+  )
+}
+
+export default CustomButton

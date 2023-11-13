69d2d1d6d048709176c5493ee81030034705d0af
siegfriedbz <siegfried.bozza@yahoo.com>
2023-11-13 19:25:18 +0100

    add Navbar Footer Logo

---
+++ components/GoogleSignIn.tsx
@@ -0,0 +1,7 @@
+'use client'
+
+const GoogleSignIn = () => {
+  return <button className='btn btn-small btn-outline'>SignIn</button>
+}
+
+export default GoogleSignIn
---
+++ components/Logo.tsx
@@ -0,0 +1,7 @@
+import Image from 'next/image'
+
+const Logo = () => {
+  return <Image src='/car-logo.svg' alt='Logo' width='32' height='32' />
+}
+
+export default Logo

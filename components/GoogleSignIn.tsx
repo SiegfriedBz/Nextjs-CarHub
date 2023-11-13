f2a70e9dac8665f2bfdd1b15d6f488d12aa436f3
siegfriedbz <siegfried.bozza@yahoo.com>
2023-11-13 20:19:16 +0100

    update GoogleSignIn

--- components/GoogleSignIn.tsx
+++ components/GoogleSignIn.tsx
@@ -1,7 +1,39 @@
 'use client'
 
+import { useSession, signIn, signOut } from 'next-auth/react'
+import CustomButton from './CustomButton'
+
 const GoogleSignIn = () => {
-  return <button className='btn btn-small btn-outline'>SignIn</button>
+  const { data: session, status } = useSession()
+  const isLoading = status === 'loading'
+  const isSignedIn = session?.user?.email != undefined
+
+  return (
+    <>
+      {isSignedIn ? (
+        <div className='flex items-center gap-2'>
+          <span>Hello {session?.user?.name?.split(' ')?.[0]}</span>
+          <CustomButton
+            handleClick={() => signOut()}
+            content='SignOut'
+            className='btn btn-small btn-outline'
+            disabled={isLoading}
+          />
+        </div>
+      ) : (
+        <CustomButton
+          handleClick={() =>
+            signIn('google', {
+              callbackUrl: process.env.NEXT_PUBLIC_NEXTAUTH_URL,
+            })
+          }
+          content='SignIn'
+          className='btn btn-small btn-outline'
+          disabled={isLoading}
+        />
+      )}
+    </>
+  )
 }
 
 export default GoogleSignIn

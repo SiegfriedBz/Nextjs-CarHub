ce2513f0acd638e667632fb359332a35b1577b50
siegfriedbz <siegfried.bozza@yahoo.com>
2023-11-16 13:36:01 +0100

    cleanup

--- constants/index.ts
+++ constants/index.ts
@@ -1,6 +1,4 @@
-import { CarType } from '@/types'
-
-export const makes: CarType['make'][] = [
+export const makes: string[] = [
   'Audi',
   'Bentley',
   'BMW',
@@ -41,8 +39,26 @@ export const makes: CarType['make'][] = [
   'Volvo',
 ]
 
-export const years: CarType['year'][] = [
-  2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023,
+type OptionType = {
+  title: string
+  value: string
+}
+
+export const years: OptionType[] = [
+  { title: 'Year', value: '' },
+  { title: '2015', value: '2015' },
+  { title: '2016', value: '2016' },
+  { title: '2017', value: '2017' },
+  { title: '2018', value: '2018' },
+  { title: '2019', value: '2019' },
+  { title: '2020', value: '2020' },
+  { title: '2021', value: '2021' },
+  { title: '2022', value: '2022' },
+  { title: '2023', value: '2023' },
 ]
 
-export const fuels: CarType['fuel_type'][] = ['Gas', 'Electricity', 'Fuel']
+export const fuels: OptionType[] = [
+  { title: 'Fuel', value: '' },
+  { title: 'Gas', value: 'Gas' },
+  { title: 'Electricity', value: 'Electricity' },
+]

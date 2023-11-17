20b43d470138b55ee6ce40e1e4d7e3cddf0d7180
siegfriedbz <siegfried.bozza@yahoo.com>
2023-11-17 08:35:59 +0100

    add ButtonMobileBurger

---
+++ components/ButtonMobileBurger.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import { useMobileMenuStore } from '@/zustand/stores'
+
+const ButtonMobileBurger = () => {
+  const { isMobileMenuOpen, toggleMobileMenu } = useMobileMenuStore()
+
+  return (
+    <button
+      id='button-mobile-burger'
+      className={`${
+        isMobileMenuOpen ? 'toggle-mobile-btn' : ''
+      } relative right-0 ms-4 h-8 w-8 rounded-2xl text-3xl`}
+      onClick={toggleMobileMenu}
+    >
+      <div
+        className={`absolute top-3.5 h-1 w-4 ${
+          !isMobileMenuOpen ? 'animate-pulse' : ''
+        } before:dark:bg-cfg-white after:dark:bg-cfg-white rounded bg-primary transition-all duration-500 before:absolute before:h-1 before:w-8 before:-translate-x-4 before:-translate-y-3 before:rounded before:bg-primary-dark before:transition-all before:duration-500 before:content-[""] after:absolute after:h-1 after:w-8 after:-translate-x-4 after:translate-y-3 after:rounded after:bg-primary-dark after:transition-all after:duration-500 after:content-[""]`}
+      ></div>
+    </button>
+  )
+}
+
+export default ButtonMobileBurger

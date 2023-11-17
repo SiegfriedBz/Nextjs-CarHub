ebac443c9db7eaeca1e37ba6864c0ae17180ae18
siegfriedbz <siegfried.bozza@yahoo.com>
2023-11-17 08:35:46 +0100

    use useMobileMenuStore

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,8 +1,16 @@
+'use client'
+
+import { useMobileMenuStore } from '@/zustand/stores'
 import Image from 'next/image'
 
 const Logo = () => {
+  const { closeMobileMenu } = useMobileMenuStore()
+
   return (
-    <div className='ring-primary-dark relative h-16 w-16 rounded-full ring-1'>
+    <div
+      onClick={closeMobileMenu}
+      className='relative h-16 w-16 rounded-full ring-1 ring-primary-dark'
+    >
       <Image src='/carhub-logo.png' fill alt='Carhub logo' />
     </div>
   )

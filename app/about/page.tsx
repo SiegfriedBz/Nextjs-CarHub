9c529aacdc75580ff1447f8c08830fb47cbc17b4
siegfriedbz <siegfried.bozza@yahoo.com>
2023-11-19 06:28:21 +0100

    update Metadata

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,4 +1,13 @@
-import React from 'react'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Car Hub | About',
+  openGraph: {
+    title: 'Car Hub | About',
+    description:
+      'Car Hub: Your Key to Seamless Car Rentals. Choose, Book, Drive – Simplifying Your Journey.',
+  },
+}
 
 const About = () => {
   return <section className='section-top'>About</section>
--- app/socials/page.tsx
+++ app/socials/page.tsx
@@ -1,4 +1,13 @@
-import React from 'react'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Car Hub | Socials',
+  openGraph: {
+    title: 'Car Hub | Socials',
+    description:
+      'Car Hub: Your Key to Seamless Car Rentals. Choose, Book, Drive – Simplifying Your Journey.',
+  },
+}
 
 const Socials = () => {
   return <section className='section-top'>Socials</section>

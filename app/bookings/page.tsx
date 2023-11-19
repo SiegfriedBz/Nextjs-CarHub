ce576c6f6edcad23ef9a8657d0f6d836a0d5eb51
siegfriedbz <siegfried.bozza@yahoo.com>
2023-11-19 07:26:54 +0100

    update BookingsPage

--- app/bookings/page.tsx
+++ app/bookings/page.tsx
@@ -3,6 +3,7 @@ import { headers } from 'next/headers'
 import { getServerSession } from 'next-auth'
 import { authOptions } from '@/utils/authOptions'
 import { redirect } from 'next/navigation'
+import Link from 'next/link'
 import BookingDetails from '@/components/BookingDetails'
 import LoadingPulse from '@/components/LoadingPulse'
 import type { BookingType } from '@/types'
@@ -16,7 +17,7 @@ export const metadata: Metadata = {
   },
 }
 
-async function getData(userEmail: string, userIsAdmin: boolean = false) {
+async function getData() {
   // fetch bookings from DB
   try {
     const response = await fetch(
@@ -33,7 +34,6 @@ async function getData(userEmail: string, userIsAdmin: boolean = false) {
     }
 
     const data = await response.json()
-    console.log(data)
     const { bookings } = data
 
     return bookings
@@ -46,7 +46,6 @@ async function getData(userEmail: string, userIsAdmin: boolean = false) {
 export default async function BookingsPage() {
   const session = await getServerSession(authOptions)
   const userEmail = session?.user?.email
-  const userIsAdmin = !!session?.user?.isAdmin
 
   // redirect if not logged in
   if (!session || !userEmail) {
@@ -54,24 +53,40 @@ export default async function BookingsPage() {
     return redirect('/signin')
   }
 
-  const bookings: BookingType[] = await getData(userEmail, userIsAdmin)
-
-  if (bookings == undefined) return <LoadingPulse />
+  const bookings: BookingType[] = await getData()
 
   return (
     <>
       <section className='section-top'>
         <h1>My Bookings</h1>
         <ul className='flex flex-col gap-y-4'>
-          {bookings?.map((booking) => {
-            return (
-              <BookingDetails
-                key={booking.id}
-                userEmail={userEmail}
-                booking={booking}
-              />
-            )
-          })}
+          {bookings == undefined ? (
+            <LoadingPulse />
+          ) : bookings && bookings.length === 0 ? (
+            <>
+              <h3 className='my-0'>You have no bookings yet.</h3>
+              <h3>
+                To start booking, please{' '}
+                <Link
+                  href='/#catalog'
+                  className='underline-gradient-link font-semibold italic'
+                >
+                  select a car
+                </Link>{' '}
+                in our catalogue.
+              </h3>
+            </>
+          ) : (
+            bookings?.map((booking) => {
+              return (
+                <BookingDetails
+                  key={booking.id}
+                  userEmail={userEmail}
+                  booking={booking}
+                />
+              )
+            })
+          )}
         </ul>
       </section>
     </>

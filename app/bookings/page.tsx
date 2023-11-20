077ca34e6844c7ba6b09bcea2af5ff1f74cd4b05
siegfriedbz <siegfried.bozza@yahoo.com>
2023-11-20 07:46:15 +0100

    fix export const dynamic = 'force-dynamic'

--- app/bookings/page.tsx
+++ app/bookings/page.tsx
@@ -1,5 +1,3 @@
-export const dynamic = 'force-dynamic'
-
 import { Metadata } from 'next'
 import BookingsList from './(components)/BookingsList'
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic'
+
 import { getServerSession } from 'next-auth'
 import { authOptions } from '@/utils/authOptions'
 import Hero from '@/components/Hero'
